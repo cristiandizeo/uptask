3d06c623933f112879e5f783ffeb23a106366de9
cristian <dizeoc@gmail.com>
2022-05-07 12:58:20 -0300

    bugs y db

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bf7b9ac0358aa2d641a93e2e7e2eb6c09a6e55db',
+    'name' => 'juandelatorre/up-task_mvc',
+  ),
+  'versions' => 
+  array (
+    'juandelatorre/up-task_mvc' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bf7b9ac0358aa2d641a93e2e7e2eb6c09a6e55db',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.6.0',
+      'version' => '6.6.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e43bac82edc26ca04b36143a48bde1c051cfd5b1',
+    ),
+  ),
+);
